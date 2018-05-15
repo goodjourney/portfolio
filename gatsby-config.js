0e9f40a08a8fdb251fb5e350f67dd40026aa2d72
Dan Beerman <omnidbeerman@gmail.com>
2018-05-14 23:54:16 -0600

    :eyes: analytics?

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,21 @@ module.exports = {
   siteMetadata: {
     title: 'Dan Beerman - Developer // Front End Engineer',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet', 
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-29961590-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+      },
+  },
+  ]
 };
